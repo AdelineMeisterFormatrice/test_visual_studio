4154e3bc43305c200c9b08267fe84b51799d70b9
Adeline <151820404+AdelineMeisterFormatrice@users.noreply.github.com>
2023-12-13 12:09:54 +0100

    ajout exo moyenne et nombre premier

---
+++ calcul_moyenne/calcul_moyenne/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Exercice 1.1 calcul de la moyenne de 2 nombre
+
+VARIABLES
+	nombre1 est un entier
+	nombre2 est un entier
+	moyenne est un reel
+DEBUT DU PROGRAMME
+	Ecrire "Saisir le 1er nombre"
+	Lire nombre1
+	Ecrire "saisir le 2eme nombre"
+	Lire nombre2
+	moyenne <-- (nombre1 + nombre2)/2d
+	Ecrire "La moyenne est :",moyenne
+FIN DU PROGRAMME
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace moyenne
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            // declaration des variables
+            int nombre1;
+            int nombre2;
+            double moyenne;
+            string saisie = "";
+
+			// Etape 1 : lecture de nombre1 et nombre2 
+			Console.WriteLine("Saisir le 1er nombre");
+			saisie = Console.ReadLine();
+			nombre1 = int.Parse(saisie);
+			Console.WriteLine("Saisir le 2eme nombre");
+			saisie = Console.ReadLine();
+			nombre2 = int.Parse(saisie);
+
+			// Etape 2 : calcul de la moyenne
+			moyenne = (nombre2 + nombre1)/2d;
+
+            Console.WriteLine("La moyenne est : " + moyenne);
+
+        }
+	}
+}
---
+++ nombrePremier/nombrePremier/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Exercice 2.6 Nombre premier
+
+Variables
+	nombre est un entier
+	i est un entier
+	compteur est un entier
+Debut du programme
+	Ecrire "Saisir un nombre entier"
+	lire nombre
+	compteur <-- 0
+	Debut boucle pour
+	Pour i allant de 2 a nombre-1
+		Debut Si
+			Si nombre mod i = 0
+			Alors
+				Compteur <-- compteur+1
+		Fin si
+	Fin pour
+	Debut si
+		Si compteur = 0
+		Alors
+			Ecrire "Le nombre ",nombre," est premier"
+		Sinon
+			Ecrire "le nombre ",nombre," n'est pas premier"
+	Fin si
+Fin du programme
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nbPremier
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int nombre;
+			int compteur;
+			string saisie;
+
+			Console.WriteLine("Saisir un nombre entier");
+			saisie = Console.ReadLine();
+			nombre = int.Parse(saisie);
+
+			compteur = 0;
+
+			for(int i = 2;i < nombre;i++)
+			{
+				if(nombre%i == 0)
+				{
+					compteur++;
+				}
+			}
+
+			if(compteur == 0)
+			{
+				Console.WriteLine("Le nombre " + nombre + " est premier");
+			}
+			else
+			{
+				Console.WriteLine("Le nombre " + nombre + " n'est pas premier");
+			}
+		}
+	}
+}
