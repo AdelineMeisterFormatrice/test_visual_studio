338eaf5a2fb407fb4dcc6e8e1db964064cfaa419
Adeline <151820404+AdelineMeisterFormatrice@users.noreply.github.com>
2023-12-13 11:29:45 +0100

    commit initial

---
+++ exemple_code_cercle/exemple_code_cercle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exemple
+{
+    class Cercle
+    {
+        static void Main(string[] args)
+        {
+            // delcaration de variables
+            string saisie;
+            double rayon;
+            double perimetre;
+
+            // Etape 1 : lecture du rayon
+            Console.WriteLine("Entrez la valeur du rayon : ");
+            saisie = Console.ReadLine();
+            // Etape 2 : calcul et affichage du perimetre
+            rayon = Convert.ToDouble(saisie);
+            perimetre = 2 * Math.PI * rayon;
+            Console.WriteLine("le cercle de rayon " + rayon);
+            Console.WriteLine(" a pour perimetre : " + perimetre);
+
+            Console.ReadLine();
+        }
+    }
+}
